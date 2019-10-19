5ead8db381d90652fbc62f97f29f9e8b4d7b99a5
Harrison Thorne <harrison.thorne@gmail.com>
2019-10-19 14:10:38 -0600

    added api interceptor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
     production: true,
-    apiProxy: {
-        target: 'https://safe-earth-09723.herokuapp.com',
-        secure: true,
-        pathRewrite: {
-            '^/api': ''
-        },
-        changeOrigin: true
-    }
+    apiURL: 'https://safe-earth-09723.herokuapp.com'
 };
