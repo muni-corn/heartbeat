302186b541ba6535e06dd11c1286dea83e80591e
municorn <municorn@musicaloft.com>
2019-10-19 15:23:13 -0600

    removed OnInit implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpService} from 'src/app/http.service';
+import { Component } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.pug',
     styleUrls: ['./login.component.styl']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
     username = '';
     password = '';
