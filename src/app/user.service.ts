541fdca13f3297b39c109a12d5e90a17e2207227
Harrison Thorne <harrison.thorne@gmail.com>
2019-10-25 23:56:41 -0600

    added register functionality

---
+++ src/app/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    constructor(private http: HttpClient) { }
+
+    public async newUser(username: string): Promise<string> {
+        return await this.http.post('/api/new_user.php', {
+            username
+        }, {
+            responseType: 'text'
+        }).toPromise();
+    }
+
+    public async getUsers(): Promise<any> {
+        return await this.http.get('/api/users.php', {
+            responseType: 'json'
+        }).toPromise();
+    }
+}
