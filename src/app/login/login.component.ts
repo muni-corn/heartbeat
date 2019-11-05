6ef49c622e93e1a6bf892c05aaeefbe4c381d7d2
municorn <municorn@musicaloft.com>
2019-11-04 21:02:11 -0700

    further development on auth pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpService } from 'src/app/http.service';
-import { UserService } from 'src/app/user.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -12,20 +11,20 @@ export class LoginComponent {
 
     username = '';
     password = '';
+    err = '';
 
     users: any;
 
     constructor(
-        private userService: UserService,
+        private authService: AuthService,
         private router: Router
     ) { }
 
     async signIn() {
-        this.users = await this.userService.getUsers();
+        this.err = await this.authService.signIn(this.username, this.password);
     }
 
     goToRegister() {
         this.router.navigate(['/auth/register']);
     }
-
 }
